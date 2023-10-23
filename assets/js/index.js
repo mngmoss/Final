1b8f9ec134e31c500d7c7a1c80a095cf7b8c2066
mngmoss <matthewngmoss@outlook.com>
2023-10-23 12:55:16 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,68 @@
+const revealText = document.getElementById("reveal-text");
+
+revealText.addEventListener("mouseenter", function () {
+  revealText.style.color = "black";
+});
+
+revealText.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+const reveal1 = document.getElementById("reveal-1");
+
+reveal1.addEventListener("mouseenter", function () {
+  reveal1.style.color = "black";
+});
+
+reveal1.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+
+const reveal2 = document.getElementById("reveal-2");
+
+reveal2.addEventListener("mouseenter", function () {
+  reveal2.style.color = "black";
+});
+
+reveal2.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+
+const reveal3 = document.getElementById("reveal-3");
+
+reveal3.addEventListener("mouseenter", function () {
+  reveal3.style.color = "black";
+});
+
+reveal3.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+
+const reveal4 = document.getElementById("reveal-4");
+
+reveal4.addEventListener("mouseenter", function () {
+  reveal4.style.color = "black";
+});
+
+reveal4.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+
+const reveal5 = document.getElementById("reveal-5");
+
+reveal5.addEventListener("mouseenter", function () {
+  reveal5.style.color = "black";
+});
+
+reveal5.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
+
+const reveal6 = document.getElementById("reveal-6");
+
+reveal6.addEventListener("mouseenter", function () {
+  reveal6.style.color = "black";
+});
+
+reveal6.addEventListener("mouseleave", function () {
+  // Do nothing here to keep the color black after hovering
+});
